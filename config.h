bde368c29ddcaf89bddf26ed76a807acf07dad52
pmihaly <pappmihalyemil@protonmail.com>
2021-04-07 09:47:41 +0200

    Control J/K to scroll

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Iosevka Term:pixelsize=17:antialias=true:autohint=true";
-static char *font2[] = {"Symbola:pixelsize=17:antialias=true:autohint=true"};
+static char *font2[] = {"JoyPixels:pixelsize=17:antialias=true:autohint=true"};
 static int borderpx = 2;
 
 
@@ -163,8 +163,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ MODKEY,               XK_k,           kscrollup,      {.i = -1} },
-	{ MODKEY,               XK_j,           kscrolldown,    {.i = -1} },
+	{ ControlMask,          XK_k,           kscrollup,      {.i = -1} },
+	{ ControlMask,          XK_j,           kscrolldown,    {.i = -1} },
 };
 
 /*
